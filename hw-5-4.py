43b32b0e5bd32c71c3cadf6de81699c5c456a522
Jordan Hart <p22jhart@fairfieldprep.org>
2021-11-02 22:33:31 -0400

    HW

---
+++ hw-5-4.py
@@ -0,0 +1,32 @@
+#question 1
+
+print("Python"[2])
+
+#question 2
+
+print("Strings are simply a sequence of characters."[30])
+
+#question 3
+
+print(len("excellent"))
+
+#question 4
+
+print("apple" in "Pinaeapple")
+
+#question 6
+
+print("x" * 100)
+
+#question 7
+
+(str(54321))
+
+#question 8
+print("p" not in "Pineapple")
+
+#question 9
+print('\"Livin\' On a Prayer\" is my friend\'s favorite song.')
+#question 10
+
+print(len('\"Livin\' On a Prayer\" is my friend\'s favorite song.'))
